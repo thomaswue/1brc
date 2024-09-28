332807ee22cfa1a233370be7f4b5a4ea530122d9
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2024-09-28 11:36:01 +0200

    Add shell scripts to measure solution 0 to 10.

---
+++ measure_all_thomaswue.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+    for ((i=0;i<=10;i++));
+    do
+	hyperfine "taskset -c 0-7 ./target/CalculateAverage_thomaswue_image_v$i"
+    done
